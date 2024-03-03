a33a26d40ac008d56e6a47da870ec1f61d6a3cea
princewilling <princewillingoo@gmail.com>
2024-03-03 19:25:36 +0100

    🥳🥳🥳🥳

--- cart/forms.py
+++ cart/forms.py
@@ -2,9 +2,19 @@
 from django import forms
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+PRODUCT_SIZE_CHOICES = [("S", "SMALL"), ("M", "MEDIUM"), ("L", "LARGE")]
 
 
 class CartAddProductForm(forms.Form):
+    size = forms.TypedChoiceField(
+        choices=PRODUCT_SIZE_CHOICES,
+        coerce=str,
+        widget=forms.Select(
+            attrs={
+                "class": "form-select appearance-none bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            }
+        ),
+    )
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
         coerce=int,
