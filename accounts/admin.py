6048250dd90f64050028b0e3a67a4e63bdb59c87
NzeStan <nnamaniifeanyi10@gmail.com>
2024-03-12 04:23:45 -0700

    import_export_latest

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from import_export.admin import ImportExportModelAdmin
 
 CustomUser = get_user_model()
 
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(ImportExportModelAdmin, UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
