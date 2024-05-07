a838f16c583c1043cb97030e80a7462aa008fffc
NzeStan <nnamaniifeanyi10@gmail.com>
2024-05-07 10:14:22 +0100

    port_exposed

--- Dockerfile
+++ Dockerfile
@@ -31,3 +31,6 @@ RUN apt-get update \
 RUN pip install --upgrade pip setuptools
 RUN pip install -r requirements.txt
 COPY . .
+
+EXPOSE 8000
+CMD ["gunicorn", "JMW.wsgi", "-b", "0.0.0.0:8000"]
--- JMW/settings.py
+++ JMW/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CSRF_TRUSTED_ORIGINS = ["https://d246-105-120-128-96.ngrok-free.app"]
 
