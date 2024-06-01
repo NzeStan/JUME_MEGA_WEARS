d10f29015c4da97362df2a739c35fc0de2cf6c48
NzeStan <nnamaniifeanyi10@gmail.com>
2024-06-01 16:33:43 -0400

    shit stressing me

--- JMW/settings.py
+++ JMW/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "nysc.apps.NyscConfig",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
+    "payment.apps.PaymentConfig",
     "import_export",
     # last
     "django_cleanup.apps.CleanupConfig",
